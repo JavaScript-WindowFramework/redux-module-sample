25bea170fdac95ed459fe1ade575cd5afb2089cb
SoraKumo <info@croud.jp>
2019-10-02 11:39:50 +0900

    ToDoサンプルを作成

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # @jswf/redux-module-sample
 
+[![npm version](https://badge.fury.io/js/%40jswf%2Fredux-module.svg)](https://badge.fury.io/js/%40jswf%2Fredux-module)
+
 Make Redux operations modular
 
-- Document
+- Document  
 [https://ttis.croud.jp/?uuid=ed418ee1-730b-40d7-b748-f2969d8d430d](https://ttis.croud.jp/?uuid=ed418ee1-730b-40d7-b748-f2969d8d430d)
+
+![screenshot.gif](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/267629/96ada761-04c4-591d-250c-3d8e41b15534.gif)
 
 ## １．Usage
 
@@ -23,7 +27,7 @@ ReactDOM.render(
 );
 ```
 
-### 1.2 Creating a store module
+### 1.2 Creating a store module class
 
 When using TypeScript, the structure of module data can be specified
 defaultState can be omitted, but in that case undefined is returned when data is accepted.
@@ -39,9 +43,6 @@ export class TestModule extends ReduxModule<State> {
   static defaultState: State = {
     msg: "init",
     count: 0,
-    other:{
-      v:123
-    }
   };
 }
 ```
@@ -100,56 +101,169 @@ class _ClassApp extends Component {
 const ClassApp = mapConnect(_ClassApp, TestModule);
 ```
 
-### 1.5 Sample
+### 1.5 Reference another class
 
 ```ts
-import React from "react";
+export interface TestState2 {
+  text: string;
+}
+export class TestModule2 extends ReduxModule<TestState2> {
+  static includes = [TestModule]; //Declare the external class used here
+  static defaultState: TestState2 = {
+    text:"A"
+  };
+  getMessage() {
+    //Call external class with getModule
+    return this.getModule(TestModule).getState("msg");
+  }
+  setMessage(msg: string) {
+    this.getModule(TestModule).setState({ msg });
+  }
+  getCount() {
+    return this.getModule(TestModule).getState("count")!;
+  }
+  setCount(count: number) {
+    this.getModule(TestModule).setState({ count });
+  }
+  addText() {
+    return this.setState(this.getText()+"a","text")!;
+  }
+  getText() {
+    return this.getState("text")!;
+  }
+}
+```
+
+### 1.6 Write only attribute
+
+ If you use the ReduxModule inheritance class in the component without specifying it, side effects will occur whenever the data under class management is updated.
+
+ If it is used only for writing, useless processing will occur. In that case, it can be avoided by attaching the writeOnly attribute.
+
+- For Function components (Hooks)  
+Set the third argument of useModule to true
+
+```ts
+const testModule = useModule(TestModule,undefined,true);
+```
+
+- For Class component  
+Set the writeOnly attribute to true at the time of mapConnect
+
+```ts
+const ClassApp = mapConnect(_ClassApp, [{module:TestModule,writeOnly:true}]);
+```
+
+- For external reference  
+
+```ts
+static includes = [{module:TestModule,writeOnly:true}];
+```
+
+### 1.7 Sample
+
+```tsx
+import React, { Component } from "react";
 import * as ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { ModuleReducer, useModule, ReduxModule } from "@jswf/redux-module";
+import {
+  ModuleReducer,
+  useModule,
+  ReduxModule,
+  mapModule,
+  mapConnect
+} from "@jswf/redux-module";
 
-//Store module data type
-export interface State {
+/**
+ *Data structure definition (when using TypeScript)
+ *
+ * @export
+ * @interface TestState
+ */
+export interface TestState {
   msg: string;
-  count: number;
 }
-export class TestModule extends ReduxModule<State> {
-  //init value
-  static defaultState: State = {
-    msg: "init",
-    count: 0
+/**
+ * Store access class
+ * (Automatically allocate space in the store for each class)
+ * @export
+ * @class TestModule
+ * @extends {ReduxModule<TestState>}
+ */
+export class TestModule extends ReduxModule<TestState> {
+  //Initial value can be set here
+  protected static defaultState: TestState = {
+    msg: "初期値"
   };
+  // It is not always necessary to create the following access methods
+  // getState and setState are public so you can rewrite directly from the outside
+  public getMessage() {
+    return this.getState("msg")!;
+  }
+  public setMessage(msg: string) {
+    this.setState({ msg });
+  }
 }
 
-function App() {
-  //Use modules
-  const module = useModule(TestModule);
-  //Get data
-  const value = module.getState()!;
+/**
+ *Sample for Hooks
+ *
+ * @returns
+ */
+function HooksApp() {
+  // Receive module instance
+  // The limit of the useModule can be used is the same as other hooks
+  const testModule = useModule(TestModule);
+  // The same class can have different areas by attaching a prefix as shown below
+  //const testModule = useModule(TestModule,"Prefix");
   return (
-    <div>
-      <div>AppComponent</div>
-      <button
-        onClick={() => {
-          //Set data
-          module.setState({ msg: "click!", count: value.count + 1 });
-        }}
-      >
-        button
-      </button>
-      <div>{value.msg}</div>
-      <div>{value.count}</div>
-    </div>
+    <>
+      <div>FunctionComponent</div>
+      <input
+        value={testModule.getMessage()}
+        onChange={e => testModule.setMessage(e.target.value)}
+      />
+      <hr />
+    </>
   );
 }
 
-//Store create
-const store = createStore(ModuleReducer);
+/**
+ *Sample for Class
+ *
+ * @class _ClassApp
+ * @extends {Component}
+ */
+class _ClassApp extends Component {
+  render() {
+    // Receive module instance
+    // Note that the name and argument are slightly different from Hooks
+    const testModule = mapModule(this.props, TestModule);
+    return (
+      <>
+        <div>ClassComponent</div>
+        <input
+          value={testModule.getMessage()}
+          onChange={e => testModule.setMessage(e.target.value)}
+        />
+        <hr />
+      </>
+    );
+  }
+}
+// When using class components, map as follows
+// Only modules declared here can be used in classes
+// Multiple modules can be specified in an array
+const ClassApp = mapConnect(_ClassApp, TestModule);
 
+// Associate a dedicated reducer with Redux
+// Can be used with other reducers
+const store = createStore(ModuleReducer);
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <HooksApp />
+    <ClassApp />
   </Provider>,
   document.getElementById("root") as HTMLElement
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,114 +1,190 @@
-import React, { Component } from "react";
+import React from "react";
 import * as ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import {
   ModuleReducer,
   useModule,
-  ReduxModule,
-  mapModule,
-  mapConnect
+  ReduxModule
 } from "@jswf/redux-module";
 
 /**
  *データ構造の定義(TypeScript使用時)
  *
  * @export
- * @interface TestState
+ * @interface TodoState
  */
-export interface TestState {
-  msg: string;
+interface TodoState {
+  //入力中データの保持
+  input: {
+    title: string;
+    desc: string;
+  };
+  //TODOリスト
+  todos: {
+    id: number;
+    title: string;
+    desc: string;
+    done: boolean;
+  }[];
+  //TODOのID附番表index
+  index: number;
 }
+
 /**
- *Storeアクセス用クラス
- *(クラスごとに自動的にStoreに領域を確保する)
+ *Todoデータ管理用クラス
+ *
  * @export
- * @class TestModule
+ * @class TodoModule
  * @extends {ReduxModule<TestState>}
  */
-export class TestModule extends ReduxModule<TestState> {
-  //ここに初期値を設定可能
-  protected static defaultState: TestState = {
-    msg: "初期値"
+export class TodoModule extends ReduxModule<TodoState> {
+  //初期値
+  protected static defaultState: TodoState = {
+    todos: [],
+    input: { title: "", desc: "" },
+    index: 0
   };
-  //以下のようなアクセス用のメソッドは、必ずしも作る必要は無い
-  //getStateとsetStateはpublicなので、外から直接書き換えてしまってもOK
-  public getMessage() {
-    return this.getState("msg")!;
+  /**
+   *ToDoリストを返す
+   *
+   * @returns
+   * @memberof TodoModule
+   */
+  public getTodos() {
+    return this.getState("todos")!;
   }
-  public setMessage(msg: string) {
-    this.setState({ msg });
+  /**
+   *ToDoを追加(追加データはStoreに入っているので引数不要)
+   *
+   * @memberof TodoModule
+   */
+  public addTodo() {
+    //indexのインクリメント
+    const index = this.getState("index")! + 1;
+    this.setState({ index });
+    //必要データの読み出し
+    const title = this.getState("input", "title")!;
+    const desc = this.getState("input", "desc")!;
+    //todoを追加
+    const todos = [
+      ...this.getState("todos")!,
+      { id: index, title, desc, done: false }
+    ];
+    this.setState({ todos })!;
+  }
+  /**
+   *ToDoの状態管理
+   *
+   * @param {number} id
+   * @param {boolean} done
+   * @memberof TodoModule
+   */
+  public updateDone(id: number, done: boolean) {
+    const srcTodos = this.getState("todos")!;
+    //状態の書き換え
+    const todos = srcTodos.map(todo =>
+      todo.id === id ? { ...todo, done } : todo
+    );
+    this.setState({ todos });
+  }
+  /**
+   *ToDoの削除
+   *
+   * @param {number} id
+   * @memberof TodoModule
+   */
+  public remove(id: number) {
+    const srcTodos = this.getState("todos")!;
+    //削除データを除外
+    const todos = srcTodos.filter(todo => todo.id !== id);
+    this.setState({ todos });
   }
 }
-/*
-export class OtherModule extends ReduxModule {
-  static includes = [TestModule]
-  public getMessage() {
-    return this.getModule(TestModule).getState("msg")!;
-  }
-  public setMessage(msg: string) {
-    this.getModule(TestModule).setState({ msg });
-  }
-}
-*/
+
+
 /**
- *Hooks用サンプル
+ *入力フォームコンポーネント
  *
  * @returns
  */
-function HooksApp() {
-  //モジュールのインスタンスを受け取る
-  //useModuleの使用可能場所の制限は他のhookと同じ
-  const testModule = useModule(TestModule);
-  //以下のようにPrefixを付けると、同じクラスが違う領域を持つことも出来る
-  //const testModule = useModule(TestModule,"Prefix");
+function FormComponent() {
+  const todoModule = useModule(TodoModule);
   return (
-    <>
-      <div>FunctionComponent</div>
-      <input
-        value={testModule.getMessage()}
-        onChange={e => testModule.setMessage(e.target.value)}
-      />
-      <hr />
-    </>
+    <div style={{ textAlign: "center" }}>
+      <div>
+        <div>タイトル</div>
+        <input
+          style={{ width: "20em" }}
+          value={todoModule.getState("input", "title")!}
+          onChange={e => todoModule.setState(e.target.value, "input", "title")}
+        />
+        <div>説明</div>
+        <textarea
+          style={{ width: "20em", height: "5em" }}
+          value={todoModule.getState("input", "desc")!}
+          onChange={e => todoModule.setState(e.target.value, "input", "desc")}
+        />
+        <div>
+          <button onClick={() => todoModule.addTodo()}>Todoを作成</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
 /**
- *Class用サンプル
+ *ToDo出力コンポーネント
  *
- * @class _ClassApp
- * @extends {Component}
+ * @returns
  */
-class _ClassApp extends Component {
-  render() {
-    //モジュールのインスタンスを受け取る
-    //Hooksと名前と引数が微妙に違うので注意
-    const testModule = mapModule(this.props, TestModule);
-    return (
-      <>
-        <div>ClassComponent</div>
-        <input
-          value={testModule.getMessage()}
-          onChange={e => testModule.setMessage(e.target.value)}
-        />
-        <hr />
-      </>
-    );
-  }
+function TodoListComponent() {
+  const todoModule = useModule(TodoModule);
+  const todos = todoModule.getState("todos")!;
+  return (
+    <div style={{ display:"flex",flexDirection: "column" ,alignItems:"center"}}>
+      {todos.map(todo => (
+        <div
+          key={todo.id}
+          style={{
+            display: "inline-block",
+            width: "20em",
+            marginTop: "1em",
+            border: "solid 1px",
+            textAlign:"center"
+          }}
+        >
+          <div>
+            {todo.id}:{todo.title}{" "}
+            <span
+              style={{ cursor: "pointer" }}
+              onClick={() => todoModule.updateDone(todo.id, !todo.done)}
+            >
+              {todo.done ? "完了" : "未完了"}
+            </span>
+            {todo.done && (
+              <span
+                style={{ cursor: "pointer" }}
+                onClick={() => todoModule.remove(todo.id)}
+              >
+                削除
+              </span>
+            )}
+          </div>
+          <div>{todo.desc}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
-//クラスコンポーネントを利用する場合は以下の方法でマッピングする
-//ここで宣言したモジュール以外はクラスで使用できない
-//モジュールは配列で複数指定も可能
-const ClassApp = mapConnect(_ClassApp, TestModule);
 
 //Reduxに専用のReducerを関連付ける
 //他のReducerと併用することも可能
 const store = createStore(ModuleReducer);
 ReactDOM.render(
   <Provider store={store}>
-    <HooksApp />
-    <ClassApp />
+    <FormComponent />
+    <TodoListComponent />
   </Provider>,
   document.getElementById("root") as HTMLElement
 );
